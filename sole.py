56e5c6f9e9143f43f1d4af75a85210287e1f89dc
Cécile Tonglet <cecile.tonglet@gmail.com>
2014-10-22 10:31:26 +0200

    Fixed: indentation

--- sole.py
+++ sole.py
@@ -3,12 +3,12 @@ import xmlrpclib
 
 def connect(url, database, login, password):
     odoo_common = xmlrpclib.ServerProxy(url + '/xmlrpc/common',
-                                            allow_none=True)
+                                        allow_none=True)
     odoo = xmlrpclib.ServerProxy(url + '/xmlrpc/object',
-                                     allow_none=True)
+                                 allow_none=True)
     uid = odoo_common.login(database, login, password)
     def make_proxy(model, method, *base_args):
         return lambda *args, **kwargs: \
             odoo.execute_kw(database, uid, password,
-                                model, method, (base_args + args), kwargs)
+                            model, method, (base_args + args), kwargs)
     return make_proxy
